ceeca181bb2b14e54bba95ae510e80c78f6f6473
shubhankar9069 <ssingha9069@gmail.com>
2024-01-31 22:20:15 +0530

    login controller added

--- controller/user.js
+++ controller/user.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 const User = require('../model/user');
 const generateToken = require('../utils/generateToken');
 
@@ -8,16 +10,16 @@ const register = async (req, res) => {
     const existingUserWithEmail = await User.findOne({ email });
     if (existingUserWithEmail) {
         return res.json({
-            'status': 'NOT_OK',
-            'message': "User already exists with this email"
+            status: 'NOT_OK',
+            message: "User already exists with this email"
         });
     }
 
     const existingUserWithPhone = await User.findOne({ phone });
     if (existingUserWithPhone) {
         return res.json({
-            'status': 'NOT_OK',
-            'message': "User already exists with this phone"
+            status: 'NOT_OK',
+            message: "User already exists with this phone"
         });
     }
 
@@ -33,21 +35,90 @@ const register = async (req, res) => {
     const token = await generateToken(newUser.id);
 
     return res.json({
-        'status': "OK",
-        "message": "You registered successfully",
-        "data": {
+        status: "OK",
+        message: "You registered successfully",
+        data: {
             'token': token
         }
     });
 };
 
 
+const login = async (req, res) => {
+    const { phone, email, password } = req.body;
+
+    try {
+        let user;
+
+        if (email) {
+            // If login is via email
+            user = await User.findOne({ email });
+        } else if (phone) {
+            // If login is via phone number
+            user = await User.findOne({ phone });
+        } else {
+            return res.status(400).json(
+                {
+                    status: "NOT_OK",
+                    message: 'Email or phone is required for login'
+                }
+            );
+        }
+
+        if (!user) {
+            return res.status(404).json({
+                status: "NOT_OK",
+                message: 'User not found'
+            }
+            );
+        }
+
+        // Compare the provided password with the hashed password stored in the database
+        const isPasswordMatch = await bcrypt.compare(password, user.password);
+
+        if (!isPasswordMatch) {
+            return res.status(401).json(
+                {
+                    status: "NOT_OK",
+                    message: 'Invalid password'
+                }
+            );
+        }
+
+        // Generate JWT token and send it in the response
+        const token = jwt.sign({ user_id: user.id }, process.env.JWT_SECRET, { expiresIn: '7d' });
+
+        res.json({
+            status: "OK",
+            message: "Logged in successfully",
+            data: {
+                token: token
+            }
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            status: "NOT_OK",
+            message: 'Internal Server Error'
+        });
+    }
+};
+
+
+
 const profile = async (req, res) => {
     const user = req.user;
-    res.json(user);
+    return res.json({
+        "status": "OK",
+        "message": "Welcome " + user.name,
+        "data": {
+            'user': user
+        }
+    })
 }
 
 module.exports = {
     register,
-    profile
+    profile,
+    login
 }
--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { register, profile } = require('../controller/user');
+const { register, profile, login } = require('../controller/user');
 const auth = require('../middleware/auth');
 const router = express.Router();
 
 router.post('/register', register);
+router.post('/login', login)
 router.get('/profile', auth ,profile);
 
 module.exports = router;
